f2f98fb3d822c949b8eda13f55b7bfb78e92c721
tomipodo <70075163+tomipodo@users.noreply.github.com>
2020-11-16 15:50:55 -0300

    .

--- src/páginaPrincipal copy.js
+++ src/páginaPrincipal copy.js
@@ -279,7 +279,7 @@ class pagPrinca extends Component{
                 this.setState({bodyPosts: posts})
                 this.setState({usuario})
                 this.setState({post: resp.data.data})
-                console.log(resp.data)
+                console.log(resp.data.data)
                 for(var i = 0; i<resp.data.length ; i++){
                     var likes = resp.data[i].id
                     if(true) {
