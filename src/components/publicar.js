4c36deb6b1f538d5fc397d2128e06094a9f5e69a
tomipodo <70075163+tomipodo@users.noreply.github.com>
2020-11-16 09:38:54 -0300

    .

--- src/components/publicar.js
+++ src/components/publicar.js
@@ -49,10 +49,15 @@ export default class Modal extends React.Component {
     axios({
       method: 'post',
       url: 'http://localhost:3000/user/',
-      headers: {
-        Authorization: "Bearer " + window.localStorage.getItem('token')
-        
-      },
+    
+        headers: [
+          { "X-localization": localStorage.getItem("lan") },
+          { "Access-Control-Allow-Origin": '*' },
+          { "Access-Control-Allow-Headers": 'Origin, X-Requested-With, Content-Type, Accept '},
+          { "Access-Control-Allow-Methods": "POST, GET, PUT, OPTIONS, DELETE" },
+          { "Access-Control-Max-Age": 3600 },
+          {Authorization: "Bearer " + window.localStorage.getItem('token')}
+        ],
       data:{
         body:"hola",
         arch_adjunto:"hola", 
